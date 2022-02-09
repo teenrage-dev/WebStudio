518f73a3818460946ea77445057b1d725164db5c
teenrflagrage <renatraiend25@gmail.com>
2022-02-09 16:30:29 +0200

    корегування

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -3,6 +3,8 @@
     openMenuBtn: document.querySelector("[data-menu-open]"),
     closeMenuBtn: document.querySelector("[data-menu-close]"),
     menu: document.querySelector("[data-menu]"),
+    modal: document.querySelector('[data-modal]'),
+    body: document.querySelector("body")
   };
 
   refs.openMenuBtn.addEventListener("click", toggleMenu);
@@ -10,6 +12,14 @@
 
   function toggleMenu() {
     refs.menu.classList.toggle("is-hidden");
+    refs.body.classList.toggle("no-scroll");
   }
+
+  window.addEventListener("resize", () => {
+    if (window.innerWidth >=768){
+      !refs.menu.classList.contains("is-hidden") ? refs.menu.classList.add("is-hidden") : "";
+      refs.body.classList.contains("no-scroll") && refs.modal.classList.contains("is-hidden") ? refs.body.classList.remove("no-scroll") : "";
+    }
+  })
 })();
   
